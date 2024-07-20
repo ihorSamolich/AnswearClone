e7d0912a15ce59eb637e5f1dda0bf5bdcad9b37e
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-07-20 19:51:49 +0300

    fix discount entity

---
+++ AnswearServer/Infrastructure/Migrations/20240720164749_Fix filters table.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixfilterstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tbl_filterValues_tbl_filterValues_FilterValueId",
+                table: "tbl_filterValues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_filterValues_FilterValueId",
+                table: "tbl_filterValues");
+
+            migrationBuilder.DropColumn(
+                name: "FilterValueId",
+                table: "tbl_filterValues");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FilterValueId",
+                table: "tbl_filterValues",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_filterValues_FilterValueId",
+                table: "tbl_filterValues",
+                column: "FilterValueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tbl_filterValues_tbl_filterValues_FilterValueId",
+                table: "tbl_filterValues",
+                column: "FilterValueId",
+                principalTable: "tbl_filterValues",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ AnswearServer/Infrastructure/Migrations/20240720164940_Fix discount table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixdiscounttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MediaFile",
+                table: "tbl_Discounts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MediaFile",
+                table: "tbl_Discounts");
+        }
+    }
+}
