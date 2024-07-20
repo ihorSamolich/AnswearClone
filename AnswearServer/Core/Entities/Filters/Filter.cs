d615e6b6a5170e34d6cdbed02b32f577522682fa
Anton Lashch <lashchanton@gmail.com>
2024-07-20 19:31:43 +0300

    Add filter tables for categories

---
+++ AnswearServer/Core/Entities/Filters/Filter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Filters
+{
+    [Table("tbl_filters")]
+    public class Filter
+    {
+
+        [ForeignKey("FilterValue")]
+        public int FilterValueId { get; set; }
+
+        public virtual FilterValue FilterValue { get; set; } = null!;
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public virtual CategoryEntity Category { get; set; } = null!;
+    }
+}
