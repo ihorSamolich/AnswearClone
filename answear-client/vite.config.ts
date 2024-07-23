526f88334582e6a654f4d70e7d8a879fd2e57a47
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-07-23 13:36:48 +0300

    install prettier and tailwind

--- answear-client/vite.config.ts
+++ answear-client/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/",
+    plugins: [react()],
+    resolve: {
+        alias: {
+            assets: "/src/assets",
+            components: "/src/components",
+            pages: "/src/pages",
+            interfaces: "/src/interfaces",
+        },
+    },
+    preview: {
+        port: 5173,
+        strictPort: true,
+    },
+    server: {
+        port: 5173,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:5173",
+    },
+});
