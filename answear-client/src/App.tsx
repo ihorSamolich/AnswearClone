6563c414362a0d2b4c49984d54eee9772be0373f
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-07-23 17:57:30 +0300

    add admin layout

--- answear-client/src/App.tsx
+++ answear-client/src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/layouts/Layout.tsx";
+
 const App = () => {
-    return <h1 className="bg-sky-300 text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route
+                    index
+                    element={
+                        <h1 className="bg-sky-300 text-3xl font-bold underline">Hello world!</h1>
+                    }
+                />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
--- answear-client/src/main.tsx
+++ answear-client/src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import React from "react";
 
@@ -7,6 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>,
 );
