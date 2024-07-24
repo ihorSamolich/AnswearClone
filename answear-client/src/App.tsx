ef7130123cd01cb6924e1f0dd869eda3c4b78139
Anton Lashch <lashchanton@gmail.com>
2024-07-24 19:11:51 +0300

    Add SignInPage on frontend

--- answear-client/src/App.tsx
+++ answear-client/src/App.tsx
@@ -1,12 +1,18 @@
 import AdminLayout from "components/layouts/AdminLayout.tsx";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "pages/HomePage.tsx";
+import LoginPage from "pages/SignInPage.tsx";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AdminLayout />}>
         <Route index element={<HomePage/>} />
+
+          <Route path="auth">
+              <Route path="sign-in" index element={<LoginPage/>} />
+          </Route>
+
       </Route>
     </Routes>
   );
