38fb7044ab20c317d73e5c9dfd37b1314aa3214a
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-07-24 11:45:51 +0300

    add front-end

---
+++ answear-client/src/components/ui/index.tsx
@@ -0,0 +1,4 @@
+import Button from "components/ui/Button.tsx";
+import ThemeToggleButton from "components/ui/ThemeToggleButton.tsx";
+
+export { Button, ThemeToggleButton };
--- answear-client/vite.config.ts
+++ answear-client/vite.config.ts
@@ -3,34 +3,28 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
-    plugins: [react()],
-    resolve: {
-        alias: {
-            assets: "/src/assets",
-            data: "/src/data",
-            components: "/src/components",
-            constants: "/src/constants",
-            hooks: "/src/hooks",
-            pages: "/src/pages",
-            app: "/src/app",
-            css: "/src/css",
-            store: "/src/store",
-            types: "/src/types",
-            utils: "/src/utils",
-            motion: "/src/motion",
-            interfaces: "/src/interfaces",
-            partials: "/src/partials",
-        },
+  base: "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+
+      components: "/src/components",
+      constants: "/src/constants",
+      css: "/src/css",
+      pages: "/src/pages",
+      utils: "/src/utils",
+      interfaces: "/src/interfaces",
     },
-    preview: {
-        port: 5173,
-        strictPort: true,
-    },
-    server: {
-        port: 5173,
-        strictPort: true,
-        host: true,
-        origin: "http://0.0.0.0:5173",
-    },
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
 });
