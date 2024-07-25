586301951ca43a45c719f453d7e22ce5448a823c
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-07-25 22:55:39 +0300

    fix bug with zod

--- answear-client/src/components/form/discount/DiscountCreateForm.tsx
+++ answear-client/src/components/form/discount/DiscountCreateForm.tsx
@@ -9,69 +9,74 @@ import { useNavigate } from "react-router-dom";
 import { useCreateDiscountMutation } from "services/discount.ts";
 
 const DiscountCreateForm = () => {
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ICreateDiscount>({ resolver: zodResolver(DiscountCreateSchema) });
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ICreateDiscount>({ resolver: zodResolver(DiscountCreateSchema) });
 
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "values",
-  });
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "values",
+    });
 
-  const navigate = useNavigate();
-  const [createDiscount, { isLoading: createDiscountIsLoading }] = useCreateDiscountMutation();
+    const navigate = useNavigate();
+    const [createDiscount, { isLoading: createDiscountIsLoading }] = useCreateDiscountMutation();
 
-  const onSubmit = async (data: DiscountCreateSchemaType) => {
-    try {
-      console.log(data);
-      // const formattedValues = data.values.map((v) => ({ value: v.value }));
-      // await createDiscount({ ...data, values: formattedValues, mediaFile: data.mediaFile[0] }).unwrap();
-      // navigate("/admin/discounts/list");
-    } catch (error) {
-      console.error("Error creating discount: ", error);
-    }
-  };
+    const onSubmit = async (data: DiscountCreateSchemaType) => {
+        try {
+            console.log(" on submit", data);
+            // const formattedValues = data.values.map((v) => ({ value: v.value }));
+            // await createDiscount({ ...data, values: formattedValues, mediaFile: data.mediaFile[0] }).unwrap();
+            // navigate("/admin/discounts/list");
+        } catch (error) {
+            console.error("Error creating discount: ", error);
+        }
+    };
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mt-8 space-y-4 border-2 border-[#dbdce0] p-8"
-      encType="multipart/form-data"
-    >
-      <div>
-        <Label htmlFor="name">Назва знижки*</Label>
-        <Input placeholder="наприклад: Супер знижка" type="text" id="name" {...register("name")} />
-        {errors.name && <Attention>{errors.name.message}</Attention>}
-      </div>
-      <div>
-        <Label htmlFor="mediaFile">Файл медіа (JPEG, PNG, MP4)*</Label>
-        <Input type="file" id="mediaFile" {...register("mediaFile")} />
-        {errors.mediaFile && <Attention>{errors.mediaFile.message}</Attention>}
-      </div>
-      <div>
-        <Label>Значення знижки*</Label>
-        {fields.map((item, index) => (
-          <div key={item.id} className="flex items-center space-x-2 mb-2">
-            <Input type="number" placeholder="наприклад: 10" {...register(`values.${index}`, { required: true })} />
-            {errors.values?.[index] && <Attention>{errors.values?.[index]?.message}</Attention>}
-            <Button type="button" onClick={() => remove(index)}>
-              -
-            </Button>
-          </div>
-        ))}
-        <Button type="button" onClick={() => append({ value: 0 })}>
-          Додати значення
-        </Button>
-        {errors.values && <Attention>{errors.values.message}</Attention>}
-      </div>
-      <div className="flex items-center justify-center">
-        <Button size="full">{createDiscountIsLoading ? <IconLoader2 className="animate-spin" /> : "Створити знижку"}</Button>
-      </div>
-    </form>
-  );
+    console.log(errors);
+
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-8 space-y-4 border-2 border-[#dbdce0] p-8"
+            encType="multipart/form-data"
+        >
+            <div>
+                <Label htmlFor="name">Назва знижки*</Label>
+                <Input placeholder="наприклад: Супер знижка" type="text" id="name" {...register("name")} />
+                {errors.name && <Attention>{errors.name.message}</Attention>}
+            </div>
+            <div>
+                <Label htmlFor="mediaFile">Файл медіа (JPEG, PNG, MP4)*</Label>
+                <Input type="file" id="mediaFile" {...register("mediaFile")} />
+                {errors.mediaFile && <Attention>{errors.mediaFile.message}</Attention>}
+            </div>
+            <div>
+                <Label>Значення знижки*</Label>
+                {fields.map((item, index) => (
+                    <div key={item.id} className="flex items-center space-x-2 mb-2">
+                        <Input type="number" placeholder="наприклад: 10" {...register(`values.${index}`)} />
+                        {errors.values?.[index] && <Attention>{errors.values?.[index]?.message}</Attention>}
+                        <Button type="button" onClick={() => remove(index)}>
+                            -
+                        </Button>
+                    </div>
+                ))}
+                <Button type="button" onClick={() => append({ value: 0 })}>
+                    Додати значення
+                </Button>
+                {errors.values?.root && <Attention>{errors.values.root?.message}</Attention>}
+                {errors.values && <Attention>{errors.values.message}</Attention>}
+            </div>
+            <div className="flex items-center justify-center">
+                <Button size="full">
+                    {createDiscountIsLoading ? <IconLoader2 className="animate-spin" /> : "Створити знижку"}
+                </Button>
+            </div>
+        </form>
+    );
 };
 
 export default DiscountCreateForm;
--- answear-client/src/interfaces/zod/discount.ts
+++ answear-client/src/interfaces/zod/discount.ts
@@ -1,54 +1,53 @@
-import { any, number, object, string, z } from "zod";
+import { z } from "zod";
 
 export const DiscountCreateSchema = z.object({
-  name: z.string().nonempty("Назва знижки обов'язкова"),
-  mediaFile: z
-    .instanceof(FileList)
-    .refine((files) => files.length > 0, "Файл медіа обов'язковий")
-    .refine((files) => {
-      try {
-        const file = files[0];
-        const allowedTypes = ["image/jpeg", "image/png", "video/mp4"];
-        return allowedTypes.includes(file.type);
-      } catch (e) {
-        return false;
-      }
-    }, "Допустимі формати файлів: JPEG, PNG, MP4"),
-  values: z
-    .array(z.any(), { message: "Значення знижки обов'язкове" })
-    .refine(
-      (sizes) => {
-        return sizes.length > 0;
-      },
-      { message: "Sizes cannot be empty" },
-    )
-    .refine((values) => {
-      console.log(values);
-      return values.every((v) => Number(v.value) != 0);
-    }, "Значення повинні бути більше 0")
-    .refine(
-      (sizes) => {
-        const sizeIds = sizes.map((size) => size);
-        const uniqueSizeIds = new Set(sizeIds);
-        return sizeIds.length === uniqueSizeIds.size;
-      },
-      { message: "Sizes must be unique" },
-    ),
+    name: z.string().nonempty("Назва знижки обов'язкова"),
+    mediaFile: z
+        .instanceof(FileList)
+        .refine((files) => files.length > 0, "Файл медіа обов'язковий")
+        .refine((files) => {
+            try {
+                const file = files[0];
+                const allowedTypes = ["image/jpeg", "image/png", "video/mp4"];
+                return allowedTypes.includes(file.type);
+            } catch (e) {
+                return false;
+            }
+        }, "Допустимі формати файлів: JPEG, PNG, MP4"),
+    values: z
+        .array(z.any(), { message: "Значення знижки обов'язкове" })
+        .refine(
+            (sizes) => {
+                return sizes.length > 0;
+            },
+            { message: "Sizes cannot be empty" },
+        )
+        .refine((values) => {
+            return values.every((v) => Number(v) > 0);
+        }, "Значення повинні бути більше 0")
+        .refine(
+            (sizes) => {
+                const sizeIds = sizes.map((size) => size);
+                const uniqueSizeIds = new Set(sizeIds);
+                return sizeIds.length === uniqueSizeIds.size;
+            },
+            { message: "Sizes must be unique" },
+        ),
 
-  // .array(
-  //   z.object({
-  //     value: z
-  //       .string()
-  //       .regex(/^\d+$/, "Значення повинно бути числом")
-  //       .transform((val) => parseFloat(val)),
-  //   }),
-  // )
-  // .nonempty("Потрібно додати хоча б одне значення")
-  // .refine((values) => values.every((v) => v.value > 0), "Значення повинні бути більше 0"),
-  // .refine((values) => {
-  //   const uniqueValues = new Set(values.map((v) => v.value));
-  //   return uniqueValues.size === values.length;
-  // }, "Значення не повинні дублюватися"),
+    // .array(
+    //   z.object({
+    //     value: z
+    //       .string()
+    //       .regex(/^\d+$/, "Значення повинно бути числом")
+    //       .transform((val) => parseFloat(val)),
+    //   }),
+    // )
+    // .nonempty("Потрібно додати хоча б одне значення")
+    // .refine((values) => values.every((v) => v.value > 0), "Значення повинні бути більше 0"),
+    // .refine((values) => {
+    //   const uniqueValues = new Set(values.map((v) => v.value));
+    //   return uniqueValues.size === values.length;
+    // }, "Значення не повинні дублюватися"),
 });
 
 export type DiscountCreateSchemaType = z.infer<typeof DiscountCreateSchema>;
