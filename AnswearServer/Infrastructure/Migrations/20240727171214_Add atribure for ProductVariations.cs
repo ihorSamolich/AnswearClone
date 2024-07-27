abe56f46cd94c342bdcccde9785cb941c616fc5d
Anton Lashch <lashchanton@gmail.com>
2024-07-27 20:18:14 +0300

    Add Atribute on ProductVariations

---
+++ AnswearServer/Infrastructure/Migrations/20240727171214_Add atribure for ProductVariations.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddatribureforProductVariations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tbl_ProductVariations_tbl_DiscountValues_DiscountValueId",
+                table: "tbl_ProductVariations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tbl_ProductVariations_tbl_DiscountValues_DiscountValueId",
+                table: "tbl_ProductVariations",
+                column: "DiscountValueId",
+                principalTable: "tbl_DiscountValues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tbl_ProductVariations_tbl_DiscountValues_DiscountValueId",
+                table: "tbl_ProductVariations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tbl_ProductVariations_tbl_DiscountValues_DiscountValueId",
+                table: "tbl_ProductVariations",
+                column: "DiscountValueId",
+                principalTable: "tbl_DiscountValues",
+                principalColumn: "Id");
+        }
+    }
+}
